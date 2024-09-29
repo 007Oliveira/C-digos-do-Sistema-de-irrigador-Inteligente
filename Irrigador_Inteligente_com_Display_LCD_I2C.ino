3fd9dfeee4b618894d5d60eeb4aa621af4b3fcac
007Oliveira <135654231+007Oliveira@users.noreply.github.com>
2024-09-29 17:01:43 -0300

    Update Irrigador_Inteligente_com_Display_LCD_I2C.ino

--- Irrigador_Inteligente_com_Display_LCD_I2C.ino
+++ Irrigador_Inteligente_com_Display_LCD_I2C.ino
@@ -10,18 +10,18 @@
 // Define o nome para pino 8 como "pinoRele"
 #define pinoRele 8
 
-// Inicializa o LCD com o endereÃ§o do mÃ³dulo e as dimensÃµes
+// Inicializa o LCD com o endereco do modulo e as dimensÃµes
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
   Serial.begin(9600);              // Define a conexÃ£o serial em 9600 bits por segundo
-  pinMode(pinoRele, OUTPUT);       // Define o pinoRele como saÃ­da
+  pinMode(pinoRele, OUTPUT);       // Define o pinoRele como saÃida
   lcd.init();                      // Inicializa o LCD
-  lcd.backlight();                 // Liga a retroiluminaÃ§Ã£o do LCD
+  lcd.backlight();                 // Liga a retroiluminacao do LCD
 }
 
 void loop() {
-  // Armazena a leitura do sensor na variÃ¡vel.
+  // Armazena a leitura do sensor na variavel.
   int Dados_sensor = analogRead(pinoAnalog);
   // Mapeia os dados do sensor para definir os valores de umidade em porcentagem.
  int Umid = map(Dados_sensor, 1023, 0, 0, 100);
@@ -32,33 +32,33 @@ Serial.print(Umid);                         // Imprime o valor da umidade no mon
 Serial.print("%");                          // Imprime no monitor serial "%."
 Serial.println();                           // Quebra uma linha no monitor serial
 
-  lcd.clear();                     // Apaga as informaÃ§Ãµes do LCD
-  lcd.setCursor(0, 1);             // Define o cursor no inÃ­cio da segunda linha do LCD
+  lcd.clear();                     // Apaga as informacoes do LCD
+  lcd.setCursor(0, 1);             // Define o cursor no inicio da segunda linha do LCD
   lcd.print("Umidade: ");          // Digita o texto "Umidade: "
   lcd.setCursor(9, 1);             // Define o cursor na nona coluna, segunda linha
   lcd.print(Umid);                 // Digita o valor de umidade
  
-  if (Umid <= 45) {                // Se a umidade for menor ou igual Ã  45% faz...
+  if (Umid <= 45) {                // Se a umidade for menor ou igual a  45% faz...
 
-    digitalWrite(pinoRele, LOW);   // Define a saÃ­da do pinoRele para nÃ­vel = LOW (Baixo), ligando a bomba
+    digitalWrite(pinoRele, LOW);   // Define a saida do pinoRele para nivel = LOW (Baixo), ligando a bomba
 
 Serial.println("Irrigando...");               // Imprime no monitor serial "Irrigando..."
 Serial.println("-------------------------");
 Serial.println();                             // Quebra uma linha no monitor serial
     
-  lcd.setCursor(0, 2);           // Define o cursor no inÃ­cio da terceira linha
+  lcd.setCursor(0, 2);           // Define o cursor no inicio da terceira linha
   lcd.print("Irrigando...");     // Digita "Irrigando..." se a umidade for baixa
   } 
 
     else {
       
-    digitalWrite(pinoRele, HIGH);  // Define a saÃ­da do pinoRele para nÃ­vel = HIGH (Alto), desligando a bomba
+    digitalWrite(pinoRele, HIGH);  // Define a saÃ­da do pinoRele para nivel = HIGH (Alto), desligando a bomba
 
 Serial.println("Pronto para irrigar.");       // Imprime no monitor serial "Pronto para irrigar."
 Serial.println("-------------------------");
 Serial.println();                             // Quebra uma linha no monitor serial
       
-  lcd.setCursor(0, 2);                   // Define o cursor no inÃ­cio da terceira linha
+  lcd.setCursor(0, 2);                   // Define o cursor no inicio da terceira linha
   lcd.print("Pronto para irrigar");      // Digita "Pronto para irrigar"
   
   }
